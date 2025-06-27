479870988e6db45078467db5e0b5cca71198a639
Willian Bruno <mestrebruno@gmail.com>
2025-06-27 04:47:50 -0300

    Salvamos a senha Bcrypt password

--- password-grant/spring-boot-3-4-3-noauth/src/main/resources/import.sql
+++ password-grant/spring-boot-3-4-3-noauth/src/main/resources/import.sql
@@ -1,5 +1,7 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$ipw31kK/vtFnJovaaZ/5bu.OhHP8dV6bGylUmrYhT1b7pBEPxFAoK');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$ipw31kK/vtFnJovaaZ/5bu.OhHP8dV6bGylUmrYhT1b7pBEPxFAoK');
+
+
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
